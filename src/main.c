616ce3dfdd394b1445078ad14815ad65e85d1bf5
jt-owo <jt-owo@outlook.com>
2025-04-12 13:27:53 +0200

    Add interrupt handler to safely exit

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <signal.h>
 
 #include "gui.h"
 #include "game.h"
+
+Game *g = NULL;
+
+static void interrupt_handler(int sign)
+{
+    g->running = !gui_confirm("Do you want to force quit? (safely)", 'y', 'n');
+    if (g->running)
+    {
+        gui_redraw(g);
+    }
+    else
+    {
+        gui_end();
+        exit(EXIT_SUCCESS);
+    }
+    
+}
 
 int main(int argc, char** argv)
 {
@@ -11,7 +29,9 @@ int main(int argc, char** argv)
 
     gui_init();
 
-    Game* g = game_new();
+    signal(SIGINT, interrupt_handler);
+
+    g = game_new();
     gui_draw(g);
     game_run(g);
     game_free(g);
